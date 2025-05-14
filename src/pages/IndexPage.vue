d678a5767825002a7498636d66099cca7c60e2e1
Ali Golmaryami <GolmaryamiA@safarayaneh.local>
2025-05-14 16:57:21 +0330

    main index changed

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -50,8 +50,14 @@ export default{
 <style lang="scss" scoped>
   .q-intersection-container{
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(5, 1fr);
     gap: 15px;
+  }
+  @media (max-width:1830px) {
+    .q-intersection-container{
+      grid-template-columns: repeat(4, 1fr);
+
+    }
   }
   @media (max-width:1530px) {
     .q-intersection-container{
